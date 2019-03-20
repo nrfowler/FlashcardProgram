a4c4d88fd8cfc80e208a8c696cfa5854e870e6a2
nrfowler <nrfowler@gmail.com>
2019-03-20 13:35:16 -0500

    Added a mental math test between tests to increase memory duration. user can now limit number of package IDs to memorize.

--- FlashcardProgram/Flashcard.cs
+++ FlashcardProgram/Flashcard.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace HelloWorld
 {
@@ -8,6 +10,47 @@ namespace HelloWorld
     {
         public String Question { get; set;}
         public String Answer { get; set;}
+        public static void FlashCards()
+        {
+            Console.WriteLine("Hello World!");
+            FileStream fs = new FileStream("data.txt", FileMode.Open);
+            Deck deck = new Deck("CSharp");
+
+            using (StreamReader sr = new StreamReader(fs))
+            {
+                String line;
+                int i = 0;
+                FlashCard card = new FlashCard();
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (i++ % 2 == 0)
+                    {
+                        //Console.WriteLine(line);
+                        card.Question = line;
+                    }
+                    else
+                    {
+                        card.Answer = line;
+                        deck.Add(card);
+                        card = new FlashCard();
+                    }
+                }
+            }
+            foreach (FlashCard c in deck.cards)
+            {
+                //Question and answer must contain one word
+                Regex rx = new Regex(@"\b([a-zA-Z]+)\b");
+
+                if (rx.IsMatch(c.Question) && rx.IsMatch(c.Answer))
+                {
+                    Console.WriteLine("\nQuestion: {0}", c.Question);
+                    Console.ReadKey();
+                    Console.WriteLine(c.Answer);
+                    Console.ReadKey();
+                }
+
+            }
+        }
 
     }
 }
--- FlashcardProgram/Program.cs
+++ FlashcardProgram/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Text;
 using System.Collections;
 using System.Text.RegularExpressions;
+using FlashcardProgram.Properties;
 
 namespace HelloWorld
 {
@@ -11,101 +12,162 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             if (args[0] == "fc")
-                FlashCards();
+                FlashCard.FlashCards();
             else if (args[0] == "wm")
-                WorkingMemory();
+                WorkingMemory(int.Parse(args[1]), int.Parse(args[2]), bool.Parse(args[3]));
+            //else if (args[0] == "mn")
+            //    PhoneticSystem();
 
         }
 
-        private static void WorkingMemory()
+        //private static void PhoneticSystem()
+        //{
+        //    var input = "n";
+        //    int level = 2, times = 0;
+        //    double average =0;
+        //    do
+        //    {
+        //        level = TestPMS(level) + 1;
+        //        GiveAverageScore(level, ref times, ref average);
+        //        Console.WriteLine("Play Again ? ");
+        //        input = Console.ReadLine();
+        //        Console.Clear();
+
+        //    } while (input != "n");
+        //}
+
+        private static double GiveAverageScore(int totalscore,  int times)
         {
-            var input="n";
-            int level = 2;
+            double average = totalscore / (double) times;
+            Console.WriteLine("Your average is " + average.ToString()+" out of "+ times.ToString()+ " games");
+            return average;
+        }
+
+        private static bool MentalMath(int level=0)
+        {
+            int top = RandomInteger(3,20,level);
+            int bottom = RandomInteger(3, 20, level);
+            Console.WriteLine("Compute: " + top.ToString() + "*" + bottom.ToString() + ": ");
+            if (int.Parse(Console.ReadLine()) == top * bottom)
+                return true;
+
+
+
+            return false;
+        }
+        
+
+        private static int RandomInteger(int start, int stop, int level)
+        {
+            return (new Random().Next() + start) % (stop + level);
+        }
+
+        private static void WorkingMemory(int total, int MaxPkgs, bool KeepScore)
+        {
+            int level = 2, times = 0;
+            int totalscore = 0;
+            if (KeepScore)
+            {
+                if ((double)Settings.Default["LastAverage"] >= (double)Settings.Default["PenultimateAverage"] + .5)
+                {
+                    total = (int)Settings.Default["LastSets"] + 1;
+                    Console.WriteLine("You have improved \nso you will have " + total.ToString() + " sets");
+
+                }
+                else if ((double)Settings.Default["LastAverage"] >= (double)Settings.Default["PenultimateAverage"] + 1)
+                {
+                    if ((int)Settings.Default["MS"] != 0)
+                        Settings.Default["MS"] = (int)Settings.Default["MS"] - 1000;
+
+                    Console.WriteLine("You have improved by a LOT \n so you will have less time to view numbers");
+
+                }
+                else
+                {
+                    total = (int)Settings.Default["LastSets"] - 1;
+                    if (total < 5)
+                        total = 5;
+
+                }
+            }
             do
             {
                 level=PlayGame(level)+1;
-                Console.WriteLine("Play Again ? ");
-                input = Console.ReadLine();
-                Console.Clear();
-            } while (input != "n");
+                level = level > MaxPkgs ? MaxPkgs : level; //function?
+                totalscore += level - 1;
+
+                if (++times != total)
+                {
+                    System.Threading.Thread.Sleep(2000);
+                    Console.Clear();
+
+                }
+            } while (times != total);
+            Console.WriteLine("Game is Over");
+            if (KeepScore)
+            {
+
+                Settings.Default["PenultimateAverage"] = Settings.Default["LastAverage"];
+                Settings.Default["LastAverage"] = GiveAverageScore(totalscore, times);
+                Settings.Default["LifetimeAverage"] = (((double)Settings.Default["LifetimeAverage"]
+                    * (int)Settings.Default["LifetimeSets"]) + totalscore) /
+                    ((int)Settings.Default["LifetimeSets"] + total);
+                Settings.Default["LifetimeSets"] = (int)Settings.Default["LifetimeSets"] + total;
+                Console.WriteLine("You have played " + Settings.Default["LifetimeSets"] + " times since March 2019");
+                Console.WriteLine("Your lifetime average is " + Settings.Default["LifetimeAverage"]);
+                Settings.Default["LastSets"] = total;
+                Settings.Default.Save();
+            }
+            Console.ReadKey();
             
 
         }
 
         private static int PlayGame(int numPackages)
         {
-            var r = new Random();
-            //var numPackages = 2;// (int)(r.NextDouble() * 10) % 4 + 2;
-            int i = 0, points = 0;
-            var PackageIDList = new ArrayList(numPackages);
-            while (i < numPackages)
-            {
-                PackageIDList.Add((int)((r.NextDouble() * 10000) % 7999+1000));
-                Console.WriteLine(PackageIDList[i++]);
-            }
-            Console.ReadKey();
-            Console.Clear();
+            int points=0;
+            ArrayList PackageIDList;
+            CreatePackageList(numPackages, out PackageIDList);
+            System.Threading.Thread.Sleep((int)Settings.Default["MS"]);
 
+
+            Console.Clear();
+            if (MentalMath(0))
+                points++;
             Console.WriteLine("Enter packages: ");
             foreach (var package in PackageIDList)
             {
                 var input = Console.ReadLine();
-                Console.Clear();
+                //Console.Clear();
 
                 if (input.Length < 4)
                     continue;
                 //.WriteLine(int.Parse(input));
-                
+
                 if ((int)package == int.Parse(input))
                     points++;
 
             }
             Console.Clear();
             Console.WriteLine("You scored " + points.ToString());
-            Console.ReadLine();
             return points;
         }
 
-        private static void FlashCards()
+        private static void CreatePackageList(int numPackages, out ArrayList PackageIDList)
         {
-            Console.WriteLine("Hello World!");
-            FileStream fs = new FileStream("data.txt", FileMode.Open);
-            Deck deck = new Deck("CSharp");
-
-            using (StreamReader sr = new StreamReader(fs))
+            var r = new Random();
+            //var numPackages = 2;// (int)(r.NextDouble() * 10) % 4 + 2;
+            int i = 0;
+            PackageIDList = new ArrayList(numPackages);
+            while (i < numPackages)
             {
-                String line;
-                int i = 0;
-                FlashCard card = new FlashCard();
-                while ((line = sr.ReadLine()) != null)
-                {
-                    if (i++ % 2 == 0)
-                    {
-                        //Console.WriteLine(line);
-                        card.Question = line;
-                    }
-                    else
-                    {
-                        card.Answer = line;
-                        deck.Add(card);
-                        card = new FlashCard();
-                    }
-                }
-            }
-            foreach (FlashCard c in deck.cards)
-            {
-                //Question and answer must contain one word
-                Regex rx = new Regex(@"\b([a-zA-Z]+)\b");
-
-                if (rx.IsMatch(c.Question) && rx.IsMatch(c.Answer))
-                {
-                    Console.WriteLine("\nQuestion: {0}", c.Question);
-                    Console.ReadKey();
-                    Console.WriteLine(c.Answer);
-                    Console.ReadKey();
-                }
+                PackageIDList.Add((int)((r.NextDouble() * 10000) % 7999 + 1000));
+                Console.WriteLine(PackageIDList[i++]);
+                System.Threading.Thread.Sleep((int)Settings.Default["MS"]);
 
             }
         }
+
+       
     }
 }
